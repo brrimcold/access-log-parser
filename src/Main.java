ab43ba36bc7db8df43f3ebe054fe59754f566d79
Kirill Petrov <brrimcold@yandex.ru>
2024-03-03 11:21:31 +0300

    added file and directory checker

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        int count = 0;
+        while (true){
+            System.out.println("Введите путь к файлу: ");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExist = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (!fileExist || isDirectory){
+                System.out.println("Был указан неверный путь к файлу или путь к папке");
+                continue;
+            }
+            else{
+                System.out.println("Путь указан верно");
+                count+=1;
+                System.out.println("Это файл номер "+count);
+                }
+             }
     }
 }
